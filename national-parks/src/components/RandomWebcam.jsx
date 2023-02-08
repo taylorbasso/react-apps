afc7525e8a6bcf8c92ced102f61ed1de78fdc604
Taylor Basso <taylorbasso@gmail.com>
2023-02-07 22:45:14 -0700

    Bug fix

--- national-parks/src/components/RandomWebcam.jsx
+++ national-parks/src/components/RandomWebcam.jsx
@@ -8,7 +8,7 @@ export default function RandomWebcam(props) {
   return (
     <div>
       <h2 className="ui center aligned header">
-        {randomWebcam.parkName} National Park
+        {randomWebcam.parkName}
       </h2>
       <div className="ui cards national-parks fluid">
         <Webcam key={randomWebcam.title} webcam={randomWebcam}/>
